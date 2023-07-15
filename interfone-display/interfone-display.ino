89756ceebacabd0f18d68dfeacee0e894ae42e3b
João Adriano Freitas <jaafreitas@gmail.com>
2023-07-15 01:29:13 -0300

    playGIF

--- interfone-display/interfone-display.ino
+++ interfone-display/interfone-display.ino
@@ -1,8 +1,10 @@
 #include <SPIFFS.h>
 #include <Arduino_GFX_Library.h>
 #include <PNGdec.h>
+#include "GifClass.h"
 
 #define PNG_FILENAME "/laroye.png"
+#define GIF_FILENAME "/laroye.gif"
 
 /*******************************************************************************
    Arduino_GFX try to find the settings depends on selected board in Arduino IDE
@@ -19,6 +21,7 @@ Arduino_GFX *gfx = new Arduino_GC9A01(bus, DF_GFX_RST, 0 /* rotation */, true /*
 
 PNG png;
 File pngFile;
+static GifClass gifClass;
 
 int16_t w, h, xOffset, yOffset;
 
@@ -26,10 +29,8 @@ void *myOpen(const char *filename, int32_t *size) {
   pngFile = SPIFFS.open(filename, "r");
   if (!pngFile || pngFile.isDirectory()) {
     Serial.println(F("ERROR: Failed to open " PNG_FILENAME " file for reading"));
-    gfx->println(F("ERROR: Failed to open " PNG_FILENAME " file for reading"));
   } else {
     *size = pngFile.size();
-    //    Serial.printf("Opened '%s', size: %d\n", filename, *size);
   }
   return &pngFile;
 }
@@ -63,6 +64,70 @@ void PNGDraw(PNGDRAW *pDraw) {
   gfx->draw16bitRGBBitmap(xOffset, yOffset + pDraw->y, usPixels, usMask, pDraw->iWidth, 1);
 }
 
+void showPNG() {
+  unsigned long start = millis();
+  int rc;
+  rc = png.open(PNG_FILENAME, myOpen, myClose, myRead, mySeek, PNGDraw);
+  if (rc == PNG_SUCCESS) {
+    int16_t pw = png.getWidth();
+    int16_t ph = png.getHeight();
+
+    xOffset = (w - pw) / 2;
+    yOffset = (h - ph) / 2;
+
+    rc = png.decode(NULL, 0);
+    png.close();
+  } else {
+    Serial.println("ERROR: png.open() failed!");
+  }
+  Serial.printf("Time used: %lu\n", millis() - start);
+}
+
+void playGif() {
+  File gifFile = SPIFFS.open(GIF_FILENAME, "r");
+  if (!gifFile || gifFile.isDirectory()) {
+    Serial.println(F("ERROR: open gifFile Failed!"));
+  } else {
+    // read GIF file header
+    gd_GIF *gif = gifClass.gd_open_gif(&gifFile);
+    if (!gif) {
+      Serial.println(F("gd_open_gif() failed!"));
+    } else {
+      uint8_t *buf = (uint8_t *)malloc(gif->width * gif->height);
+      if (!buf) {
+        Serial.println(F("buf malloc failed!"));
+      } else {
+        int16_t x = (gfx->width() - gif->width) / 2;
+        int16_t y = (gfx->height() - gif->height) / 2;
+
+        int32_t t_fstart, t_delay = 0, t_real_delay, delay_until;
+        int32_t res = 1;
+        int32_t duration = 0, remain = 0;
+        while (res > 0) {
+          t_fstart = millis();
+          t_delay = gif->gce.delay * 10;
+          res = gifClass.gd_get_frame(gif, buf);
+          if (res < 0) {
+            break;
+          } else if (res > 0) {
+            gfx->drawIndexedBitmap(x, y, buf, gif->palette->colors, gif->width, gif->height);
+
+            t_real_delay = t_delay - (millis() - t_fstart);
+            duration += t_delay;
+            remain += t_real_delay;
+            delay_until = millis() + t_real_delay;
+            while (millis() < delay_until) {
+              delay(1);
+            }
+          }
+        }
+        gifClass.gd_close_gif(gif);
+        free(buf);
+      }
+    }
+  }
+}
+
 void setup() {
   Serial.begin(115200);
 
@@ -73,33 +138,17 @@ void setup() {
   gfx->fillScreen(WHITE);
   w = gfx->width(), h = gfx->height();
 
-#ifdef GFX_BL
   pinMode(GFX_BL, OUTPUT);
   digitalWrite(GFX_BL, HIGH);
-#endif
 
   if (!SPIFFS.begin()) {
     Serial.println(F("ERROR: file system mount failed!"));
   } else {
-    unsigned long start = millis();
-    int rc;
-    rc = png.open(PNG_FILENAME, myOpen, myClose, myRead, mySeek, PNGDraw);
-    if (rc == PNG_SUCCESS) {
-      int16_t pw = png.getWidth();
-      int16_t ph = png.getHeight();
-
-      xOffset = (w - pw) / 2;
-      yOffset = (h - ph) / 2;
-
-      rc = png.decode(NULL, 0);
-      png.close();
-    } else {
-      Serial.println("ERROR: png.open() failed!");
-    }
-    Serial.printf("Time used: %lu\n", millis() - start);
+    showPNG();
+    delay(5000);       
   }
 }
 
 void loop() {
-  //
+  playGif();
 }
